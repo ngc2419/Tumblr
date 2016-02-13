eaa80632765eac2c54f6fe334642c825e3898016
ngc2419 <ngc2419@users.noreply.github.com>
2016-02-13 06:27:36 -0500

    Create infiniteScrolling.js

---
+++ infiniteScrolling.js
@@ -0,0 +1,13 @@
+<!-- JS for infinte scrolling -->
+
+<script type="text/javascript" src="http://codysherman.com/tools/infinite-scrolling/code"></script>
+
+
+<div>
+{block:IfEndlessScrolling}<div class="autopagerize_page_element">{/block:IfEndlessScrolling}
+
+<!-- endless scrolling -->
+{block:IfEndlessScrolling}</div>{/block:IfEndlessScrolling}
+{block:IfNotEndlessScrolling}
+{block:Pagination}
+
